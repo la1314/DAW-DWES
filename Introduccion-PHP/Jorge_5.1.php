4bf10c03f9d8010c65d22a187e762de28ddfea30
la1314 <tsue00@gmail.com>
2019-09-30 17:55:47 +0200

    Añadiendo ejercicio 5.1

--- Introduccion-PHP/Jorge_5.1.php
+++ Introduccion-PHP/Jorge_5.1.php
@@ -5,6 +5,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Jorge 5.1</title>
+
+    <style>
+
+    th {
+        color: red;
+        background-color: yellow;
+    }
+
+    td {
+        background-color: aquamarine;
+    }
+    
+    table, th, td {
+        border: 1px solid black;
+        border-collapse: collapse;
+        padding: 5px;
+        
+    }
+
+    </style>
+
 </head>
 <body>
     
@@ -12,13 +33,63 @@
 
     $iterante = 1;
 
+    echo("<table>");
+    echo("<th>Número</th>");
+    echo("<th>Tipo</th>");
+    echo("<th>Primo</th>");
+
     while( $iterante < 11 ){
 
+        echo("<tr>");
+
+        echo("<td>$iterante</td>");
+
+        if ($iterante%2 == 0) {
+
+            echo("<td>Par</td>");
+
+        } else {
+
+            echo("<td>Impar</td>");
+        
+        }
+        
+        if (esPrimo($iterante)) {
+
+            echo("<td>Es primo</td>");
+
+        } else {
+
+            echo("<td>No es primo</td>");
+        
+        }
+       
+        
+        echo("</tr>");
 
 
 
         $iterante++;
     }
+
+    function esPrimo($number) {
+        $cont=0;
+        
+        for($i = 2; $i <= $number; $i++) {
+
+            if($number % $i == 0) {
+                
+                if(++$cont > 1)
+
+                    return false;
+
+            }
+        }
+        
+        return true;
+    }
+
+    echo("</table>");
 
     ?>
 
