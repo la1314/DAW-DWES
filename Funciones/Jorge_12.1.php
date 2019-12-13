23f4f7fdfb04bf6c9d2347c7f37b615e9ff2f44b
la1314 <tsue00@gmail.com>
2019-12-13 14:43:28 +0100

    Añadiendo ejercicio de funciones

--- Funciones/Jorge_12.1.php
+++ Funciones/Jorge_12.1.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <title>
-        Simulador de paloma mensajera
+        Jorge 12.1 Simulador de paloma mensajera
     </title>
     <link rel="stylesheet" type="text/css" href="common.css" />
     <style type="text/css">
@@ -37,22 +37,21 @@
     <h1>
         <?php
 
-        $tamañoMapa = 10;
+        #Devuelve una posición aleatoria dependiendo del tamaño del mapa
+        function asignarPosicion($tamañoMapa)
+        {
+            return rand(0, $tamañoMapa - 1);
+        }
 
-        # Posicionar la paloma
+        #Función que comprueba que la distancia entre la paloma y la casa disten como mínimo la mitad del mapa
+        function comprobarDistancia($casa, $paloma, $tamañoMapa){
 
-        do {
-            $casaX = rand(0, $tamañoMapa - 1);
-            $casaY = rand(0, $tamañoMapa - 1);
-            $palomaX = rand(0, $tamañoMapa - 1);
-            $palomaY = rand(0, $tamañoMapa - 1);
-        } while ((abs($casaX - $palomaX) < $tamañoMapa / 2) && (abs($casaY - $palomaY) < $tamañoMapa / 2));
-        # Con la línea precedente nos aseguramos de que la posición inicial de la paloma
-        # y su casa disten como mínimo la mitad del mapa. La función abs() devuelve el valor
-        # absoluto, esto es, la cifra sin signo.
+            return (abs($casa - $paloma) < $tamañoMapa / 2);
+        }
 
-        do {
-            #Acercar la paloma a su casa
+        #Función que acerca a la paloma a su casa utiliza referencias de variables como parámetros
+        function acercarPaloma(&$palomaX, &$casaX, &$palomaY, &$casaY)
+        {
             if ($palomaX < $casaX)
                 $palomaX++;
 
@@ -64,9 +63,12 @@
                 $palomaY++;
             elseif ($palomaY > $casaY)
                 $palomaY--;
+        }
 
+        #Función que dibuja el mapa utilizando los valores de las variables pasadas como parámetros
+        function dibujarMapa($tamañoMapa, $casaX, $palomaX, $casaY, $palomaY)
+        {
 
-            #Mostrar el mapa actual
             echo '<div class="map" style="width: ' . $tamañoMapa . 'em;"><pre>';
             # Recuérdese que con la etiqueta <pre> los saltos de línea que haya se reflejan en la pantalla
 
@@ -84,9 +86,38 @@
                 }
 
                 echo "\n"; #Salto de línea. como se está dentro de un <pre>, se reflejará en la pantalla.
+
             }
 
             echo "</pre>palomax=$palomaX palomaY=$palomaY</div>\n";
+        }
+
+        // Apartado de variables
+
+        $tamañoMapa = 10;
+
+        # Posicionar la paloma
+
+        do {
+
+            $casaX = asignarPosicion($tamañoMapa);
+            $casaY = asignarPosicion($tamañoMapa);
+            $palomaX = asignarPosicion($tamañoMapa);
+            $palomaY = asignarPosicion($tamañoMapa);
+
+        } while ( comprobarDistancia($casaX, $palomaX, $tamañoMapa) && comprobarDistancia($casaY, $palomaY, $tamañoMapa) );
+        # Con la línea precedente nos aseguramos de que la posición inicial de la paloma
+        # y su casa disten como mínimo la mitad del mapa. La función abs() devuelve el valor
+        # absoluto, esto es, la cifra sin signo.
+
+        do {
+
+            #Acercar la paloma a su casa 
+            acercarPaloma($palomaX, $casaX, $palomaY, $casaY);
+
+            #Mostrar el mapa actual
+            dibujarMapa($tamañoMapa, $casaX, $palomaX, $casaY, $palomaY);
+            
         } while ($palomaX != $casaX || $palomaY != $casaY);
 
         ?>
